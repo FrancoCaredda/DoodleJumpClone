42022559e3c956ff5d0cb1f199a309faa4ff6953
FrancoCaredda <franco.caredda12345@gmail.com>
2025-06-22 22:44:39 +0200

    Refactoring

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
 #include "raylib.h"
+#include "raymath.h"
 
 #include <cstdint>
 #include <cstdlib>
@@ -59,7 +60,7 @@ void Game::RunLoop()
 		BeginDrawing();
 		ClearBackground(Color{});
 
-		UpdateLogic(m_Enitities, GetFrameTime());
+		UpdateLogic(GetFrameTime());
 		m_Renderer.RenderBackground();
 		m_Renderer.RenderEntities(m_Enitities, m_Spritesheet);
 		m_Renderer.RenderRectangles(m_Enitities);
@@ -72,4 +73,81 @@ void Game::RunLoop()
 void Game::Close()
 {
 	CloseWindow();
+}
+
+void Game::UpdateLogic(float deltaTime)
+{
+	bool collision = false;
+	Vector2 movement = Vector2{ 0.0f, 0.0f };
+	for (Entity& entity : m_Enitities)
+	{
+		switch (entity.Type)
+		{
+		case EntityType::Character:
+			collision = CheckCollision_Character(entity);
+			movement = GetMovement_Character();
+			Update_Character(entity, movement, collision, deltaTime);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void Game::Update_Character(Entity& entity, const Vector2& movementDirection, bool collision, float deltaTime)
+{
+	Vector2 newAcceleration = Vector2Add(entity.Acceleration, movementDirection);
+	Vector2 currentAcceleration = Vector2Scale(newAcceleration, deltaTime);
+
+	if (collision)
+	{
+		Vector2 newVelocity = Vector2Add(Vector2{ entity.Velocity.x, -0.25f }, currentAcceleration);
+		Vector2 newPosition = Vector2Add(entity.Position, newVelocity);
+
+		entity.Velocity = newVelocity;
+		entity.Position = newPosition;
+		return;
+	}
+
+	Vector2 newVelocity = Vector2Add(entity.Velocity, currentAcceleration);
+	Vector2 newPosition = Vector2Add(entity.Position, newVelocity);
+
+	entity.Velocity = newVelocity;
+	entity.Position = newPosition;
+}
+
+bool Game::CheckCollision_Character(Entity& character)
+{
+	for (int i = 0; i < m_Enitities.size(); i++)
+	{
+		if (character.Type == m_Enitities[i].Type)
+		{
+			continue;
+		}
+
+		if (CheckCollisionRecs(character.GetRectangle(), m_Enitities[i].GetRectangle()) &&
+			character.Solid && m_Enitities[i].Solid &&
+			character.Position.y < m_Enitities[i].Position.y)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+Vector2 Game::GetMovement_Character()
+{
+	static Vector2 Right{ 0.5f, 0.0f };
+
+	if (IsKeyDown(KEY_D))
+	{
+		return Right;
+	}
+	if (IsKeyDown(KEY_A))
+	{
+		return Vector2Scale(Right, -1);
+	}
+
+	return Vector2Scale(Right, 0.0f);
 }
--- Game/src/Game.h
+++ Game/src/Game.h
@@ -19,6 +19,11 @@ private:
 	void Init();
 	void RunLoop();
 	void Close();
+
+	void UpdateLogic(float deltaTime);
+	void Update_Character(Entity& entity, const Vector2& movementDirection, bool collision, float deltaTime);
+	bool CheckCollision_Character(Entity& character);
+	Vector2 GetMovement_Character();
 private:
 	Spritesheet m_Spritesheet;
 	Renderer m_Renderer{1200, 900};
